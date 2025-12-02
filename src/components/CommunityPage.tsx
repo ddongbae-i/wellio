e2d2ed20c07b00b46e33e7fea7b0749b098f8c5b
ddongbae-i <viragomk@gmail.com>
2025-12-02 12:18:28 +0900

    12:18

--- src/components/CommunityPage.tsx
+++ src/components/CommunityPage.tsx
@@ -882,7 +882,7 @@ export function CommunityPage({
         ) : (
           <div
             ref={scrollContainerRef}
-            className={`w-full px-5 xs:px-6 sm:px-8 overflow-y-auto h-full scrollbar-hide snap-y snap-mandatory ${isKeyboardVisible ? "pb-40" : ""
+            className={`w-full px-5 xs:px-6 sm:px-8 overflow-y-auto h-full scrollbar-hide snap-y snap-mandatory ${isKeyboardVisible ? "pb-30" : ""
               }`}
           >
             {filteredPosts.map((post) => {
