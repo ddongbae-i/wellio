3ae83b17de5677ebf99626951af72afd6ef93263
ddongbae-i <viragomk@gmail.com>
2025-12-02 11:12:03 +0900

    11:12

--- src/components/CommunityPage.tsx
+++ src/components/CommunityPage.tsx
@@ -895,7 +895,7 @@ export function CommunityPage({
                   className={`flex flex-col items-center w-full gap-4 py-5 xs:py-6 sm:py-8
     ${!isKeyboardVisible
                       ? "justify-center snap-start snap-always"
-                      : "justify-center"
+                      : "justify-start pb-300px"
                     }
   `}
                   key={post.id}
