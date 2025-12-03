125409280939add167aaa090887db2fae29b0e19
ddongbae-i <viragomk@gmail.com>
2025-12-03 14:46:36 +0900

    14:46

--- src/components/UploadPage.tsx
+++ src/components/UploadPage.tsx
@@ -593,7 +593,7 @@ export function UploadPage({ onBack, onUpload }: UploadPageProps) {
       }}
       className="fixed left-1/2 -translate-x-1/2 z-[100] w-full max-w-[500px] bg-white rounded-t-[16px] shadow-[0_-2px_5px_0_rgba(0,0,0,0.10)]"
       style={{
-        bottom: isKeyboardVisible ? keyboardHeight + 20 : 0,
+        bottom: isKeyboardVisible ? 100 : 0,
         paddingBottom: "env(safe-area-inset-bottom)",
       }}
     >
